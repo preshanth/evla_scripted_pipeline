f73483400cacf41b4781bca6762e6a2720a1987c
Brian Svoboda <bsvoboda@nrao.edu>
2023-01-27 13:52:15 -0700

    Update restore

--- evla_pipe/EVLA_pipe_restart.py
+++ evla_pipe/EVLA_pipe_restart.py
@@ -1,65 +1,58 @@
-######################################################################
-#
-# Copyright (C) 2013
-# Associated Universities, Inc. Washington DC, USA,
-#
-# This library is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Library General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at your
-# option) any later version.
-#
-# This library is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
-# License for more details.
-#
-# You should have received a copy of the GNU Library General Public License
-# along with this library; if not, write to the Free Software Foundation,
-# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
-#
-# Correspondence concerning VLA Pipelines should be addressed as follows:
-#    Please register and submit helpdesk tickets via: https://help.nrao.edu
-#    Postal address:
-#              National Radio Astronomy Observatory
-#              VLA Pipeline Support Office
-#              PO Box O
-#              Socorro, NM,  USA
-#
-######################################################################
+"""
+Facilitates the restart of the pipeline. The `EVLA_pipe_restore.py` script
+needs to be run before this one.
+"""
 
-'''
-Facilitates the restart of the pipeline.
-EVLA_pipe_restore.py needs to be run before hand
+from . import exec_script
+from .utils import logprint
 
-'''
 
-pipeline_scripts = ['startup', 'import', 'hanning', 'msinfo', 'flagall',
-                    'calprep', 'priorcals', 'testBPdcals',
-                    'flag_baddeformatters', 'flag_baddeformattersphase',
-                    'checkflag', 'semiFinalBPdcals1',
-                    'checkflag_semiFinal', 'semiFinalBPdcals2',
-                    'solint', 'testgains', 'fluxgains', 'fluxboot',
-                    'finalcals', 'applycals', 'targetflag',
-                    'statwt', 'plotsummary', 'filecollect', 'weblog']
+pipeline_scripts = [
+    "startup",
+    "import",
+    "hanning",
+    "msinfo",
+    "flagall",
+    "calprep",
+    "priorcals",
+    "testBPdcals",
+    "flag_baddeformatters",
+    "flag_baddeformattersphase",
+    "checkflag",
+    "semiFinalBPdcals1",
+    "checkflag_semiFinal",
+    "semiFinalBPdcals1",
+    "solint",
+    "testgains",
+    "fluxgains",
+    "fluxboot",
+    "finalcals",
+    "applycals",
+    "targetflag",
+    "statwt",
+    "plotsummary",
+    "filecollect",
+    "weblog",
+]
 
-#last script that was started
-last_state = time_list[-1]['pipestate']
-last_status = time_list[-1]['status']
+# Last script that was started
+last_state = time_list[-1]["pipestate"]
+last_status = time_list[-1]["status"]
 
-#Find index where we need to pick up at
+# Find index where we need to pick up at
 script_index = [i for i, x in enumerate(pipeline_scripts) if last_state == x][0]
 
-#If script ended successfully then start on next script
-if (last_status == 'end'):
-    script_index = script_index + 1
+# If script ended successfully then start on next script
+if last_status == "end":
+    script_index += 1
 
-if (last_status == 'start'):
+if last_status == "start":
     time_list.pop(-1)
 
-for i in range( script_index, len(pipeline_scripts) ):
+for script_name in pipeline_scripts[script_index:]:
     try:
-        execfile(pipepath+'EVLA_pipe_'+pipeline_scripts[i]+'.py')
-    except Exception, e:
-        logprint ("Exiting script: "+str(e))
-    except KeyboardInterrupt, e:
-        logprint ("Keyboard Interrupt: "+str(e))
+        exec_script(f"EVLA_pipe_{script_name}", globals())
+    except Exception as e:
+        logprint(f"Exiting script: {e}")
+    except KeyboardInterrupt as e:
+        logprint(f"Keyboard Interrupt: {e}")
